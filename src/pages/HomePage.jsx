25ac4ca077ceaf283e1a4068728f4e3a4e21065f
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-10-03 10:13:29 +0200

    Commit test Bjarke

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Home Page</h1>
+      <p>Home is where the heart is 💛</p>
+      <p>Oh My, sounds like a bad movie!</p>
+
+      <h1>Bjarkes test</h1>
+    </section>
+  );
 }
