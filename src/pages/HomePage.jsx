2c01e8ba0502cb366f7cc0f7eaf61d1048b86acb
Albert Hetting <Albertlund@hotmail.dk>
2025-10-03 10:36:05 +0200

    small fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return (
         <section className="page">
             <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
+            <p>Home is where the heart 💛</p>
             <p>Oh My, sounds like a bad movie!</p>
         </section>
     );
