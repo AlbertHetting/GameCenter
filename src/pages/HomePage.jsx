b505b194447d9aff9dced473bf6eaf07db351849
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-10-06 08:34:22 +0200

    new test feature - bjarke

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart </p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Home Page</h1>
+      <p>Home is where the heart </p>
+      <p>Oh My, sounds like a bad movie!</p>
+      <h1>This is my new feature - testing merging branches</h1>
+    </section>
+  );
 }
