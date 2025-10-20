1cc48cb4dec8b23628a393a4d8018644ac8c6749
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-10-20 11:17:12 +0200

    Adding background to splash

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,7 +1,10 @@
 export default function Splash() {
   return (
-    <main className="bg-black">
-      <h1 className="">This is the splash screen</h1>
+    <main className="bg-[url('/img/Background1.png')] flex h-screen w-screen items-center justify-center bg-cover bg-center">
+      <div className="flex flex-col">
+        <img src="public/img/GameSquareLogo2.svg" alt="" />
+        <h1 className="">Loading...</h1>
+      </div>
     </main>
   );
 }
