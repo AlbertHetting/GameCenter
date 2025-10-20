bb88b94909f2ad6b8dba16c0ab31a1a4e019d263
Albert Hetting <Albertlund@hotmail.dk>
2025-10-20 10:54:43 +0200

    Fixed host on network

--- src/pages/QuickGuide.jsx
+++ src/pages/QuickGuide.jsx
@@ -1,10 +1,19 @@
-
+import "./Guide.css";
 
 export default function QuickGuide(){
     return(
-        <div>
-        <img className="Concept" src="img/Concept.png" alt="" />
-        <h1>HELLO!</h1>
+        <section>
+
+        <div className="conceptheader">
+        <div >
+        <img className="Concept" src="/img/Concept.png" alt="" />
         </div>
+        </div>
+
+
+        <div className="textcon">
+        <h1>Games have rules here's how to play</h1>
+        </div>
+        </section>
     )
 }
