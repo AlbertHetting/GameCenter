0b6bf15708502e06db25fc901d9e8ab2170f0914
Albert Hetting <Albertlund@hotmail.dk>
2025-10-22 12:13:21 +0200

    patching 1

--- src/pages/QuickGuide.jsx
+++ src/pages/QuickGuide.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export default function QuickGuide(){
     return(
-         <body id="QuickGuide">
+    <body>
              
        <main>
         <article>
