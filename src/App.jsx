264e42259102f9cd9481c1b0dce0292914a209a3
Albert Hetting <Albertlund@hotmail.dk>
2025-10-24 10:44:58 +0200

    Authentication pathing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Browse from "./pages/Browse";
 import LoginProfileMain from "./pages/LoginProfileMain";
 import JoinGame from "./pages/JoinGameCode";
+import RequireAuth from "./components/RequireAuth";
 
 export default function App() {
   return (
@@ -15,8 +16,8 @@ export default function App() {
           <Route path="guide" element={<QuickGuide />} />
           <Route path="profile" element={<Profile />} />
           <Route path="login" element={<LoginProfileMain />} />
-          <Route path="browse" element={<Browse />} />
-          <Route path="Join" element={<JoinGame />} />
+          <Route path="/browse" element={<RequireAuth> <Browse /> </RequireAuth>}/>
+          <Route path="/join" element={<RequireAuth> <JoinGame /> </RequireAuth>}/>
         </Routes>
       </main>
     </>
