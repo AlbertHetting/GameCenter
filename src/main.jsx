31aa9e3d77ff26cadb2702d83926de63c123e67d
Albert Hetting <Albertlund@hotmail.dk>
2025-10-24 09:35:47 +0200

    Potential image fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/GameCenter/"}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
